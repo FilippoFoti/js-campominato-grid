d185afc7ffbecdffa7fb3f39aa09589bd23cc16c
FilippoFoti <filippofoti7@gmail.com>
2023-03-17 18:40:29 +0100

    prova1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,91 @@
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di 
-// azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Dichiaro le varie difficoltà e la quantità di casellle
+const boxEasy = 100;
+const boxMedium = 81;
+const boxHard = 49;
+
+const playBtn = document.querySelector('.play-btn');
+playBtn.addEventListener("click", function() {
+
+    const gridBox = document.querySelector(".grid");
+
+    // Prendo il valore del input.
+    const userChoice = document.getElementById("difficulty");
+    const userChoiceInput = userChoice.value;
+    console.log(userChoice)
+
+    if (userChoiceInput === "easy") {
+        // Creo la griglia al click del tasto Play
+        // playBtn.addEventListener('click', gridEasy);
+
+        // Genero la griglia e caselle (100)
+        function gridEasy() {
+            for (i = 1; i <= 100; i++){
+                const grid = generateGridItem(i);
+                grid.addEventListener("click", colorClick)
+                gridBox.append(grid);
+            }
+        }
+        gridEasy()
+
+        function generateGridItem(text) {
+            const newSquare = document.createElement("div");
+            newSquare.classList.add("grid-item");
+            newSquare.classList.add("easy")
+            newSquare.innerHTML = `<span>${text}</span>`;
+            return newSquare;
+        }
+    } else if (userChoiceInput === "medium") {
+        // Creo la griglia al click del tasto Play
+        // playBtn.addEventListener('click', gridMedium);
+
+        // Genero la griglia e caselle (100)
+        function gridMedium() {
+            for (i = 1; i <= 81; i++){
+                const grid = generateGridItem(i);
+                grid.addEventListener("click", colorClick)
+                gridBox.append(grid);
+            }
+        }
+        gridMedium()
+        
+
+        function generateGridItem(text) {
+            const newSquare = document.createElement("div");
+            newSquare.classList.add("grid-item");
+            newSquare.classList.add("medium")
+            newSquare.innerHTML = `<span>${text}</span>`;
+            return newSquare;
+        }
+    } else if (userChoiceInput === "hard") {
+         // Creo la griglia al click del tasto Play
+        //  playBtn.addEventListener('click', gridHard);
+
+        // Genero la griglia e caselle (100)
+        function gridHard() {
+            for (i = 1; i <= 49; i++){
+                const grid = generateGridItem(i);
+                grid.addEventListener("click", colorClick)
+                gridBox.append(grid);
+            }
+        }
+        gridHard()
+
+        function generateGridItem(text) {
+            const newSquare = document.createElement("div");
+            newSquare.classList.add("grid-item");
+            newSquare.classList.add("hard")
+            newSquare.innerHTML = `<span>${text}</span>`;
+            return newSquare;
+        }
+    }
+
+    // Ripulisco i campi di input
+    userChoice.value = "";
+});
+
+// La casella si colora al click
+function colorClick() {
+    const clickedNumber = parseInt(this.querySelector("span").textContent);
+    this.classList.toggle("blue");
+    console.log(clickedNumber);
+}
